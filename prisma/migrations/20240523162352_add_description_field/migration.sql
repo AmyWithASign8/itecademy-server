c67463681856bf8649a0946be54c1aa9fdd20e1d
DanilFrontend <danilstorage@gmail.com>
2024-05-23 23:01:45 +0500

    🗃️ db: added `description` field in `services` model

---
+++ prisma/migrations/20240523162352_add_description_field/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `services` table. All the data in the column will be lost.
+  - Added the required column `description` to the `services` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `title` to the `services` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "services" DROP COLUMN "name",
+ADD COLUMN     "description" TEXT NOT NULL,
+ADD COLUMN     "title" VARCHAR(255) NOT NULL;
